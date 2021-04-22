55461e5c926bb96f8e9bb737b4c2f3d332449cf7
MariaSanchezQ <>
2021-04-22 19:45:14 +0000

    Reto4 Listo

---
+++ py2_retos1/reto4.py
@@ -0,0 +1,9 @@
+a = int(input("Escriba el primer numero: "))
+b = int(input("Escriba el segundo numero: "))
+
+if (a>b):
+  print("El numero mayor es: "+str(a))
+elif (b>a):
+  print("El numero mayor es: "+str(b))
+else:
+  print("Los numeros son iguales")
