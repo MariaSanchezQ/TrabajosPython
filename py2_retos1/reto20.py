2bfdc9c701826aab4003903ae9f4aae52059a831
MariaSanchezQ <>
2021-04-22 20:25:22 +0000

    Reto20 Listo (Corregido)

--- py2_retos1/reto20.py
+++ py2_retos1/reto20.py
@@ -1,9 +1,8 @@
 def bisiesto(a):
-  b = a%4
-  if(b == 0):
-    print("El año es bisiesto")
+  if(a%4 == 0 and (not(a%100==0) or a%400==0)):
+    print("El año", str(a),"es bisiesto")
   else:
-    print("El año no es bisiesto")
+    print("El año",str(a)," no es bisiesto")
 
 
 a= int(input("Escriba el año a evaluar: "))
