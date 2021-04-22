6280d1a3f8be2963ddf20a9a5310f37c362fa1fb
MariaSanchezQ <>
2021-04-22 19:40:53 +0000

    Reto3 Listo

---
+++ py2_retos1/reto3.py
@@ -0,0 +1,14 @@
+# Operaciones con Numeros
+
+a = 30
+b = 15
+
+suma= a+b
+resta = a-b
+multiplicacion = a*b
+division = a/b
+
+print("La suma de a y b es: "+str(suma))
+print("La resta de a y b es: "+str(resta))
+print("La multiplicacion de a y b es: "+ str(multiplicacion))
+print("La division de a y b es: "+str(division))
