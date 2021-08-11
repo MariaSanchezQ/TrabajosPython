32fb1b8de3534d41c8718d85ebe5e12f7beb648e
MariaSanchezQ <81332618+MariaSanchezQ@users.noreply.github.com>
2021-08-11 23:09:31 +0000

    Creación lista vacía

--- py4_comprension/palabras.py
+++ py4_comprension/palabras.py
@@ -1,4 +1,4 @@
-A=['gato','perro','amarillo','naranja','pera','manzana','halla']
+A=['gato','perro','amarillo','naranja','pera','manzana','halla','mazorco','mazorca','alla']
 
 a =[i for i in A if(i[-1]=='a')]
 print(a)
---
+++ py5_matplotlib/plot_list.py
@@ -0,0 +1,4 @@
+C=[];
+a=int(input("Ingrese un valor entero: "));
+C.append(a);
+print(C);
