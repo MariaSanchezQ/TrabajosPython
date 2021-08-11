823acbc7e02ab50500f93edc57e7232d332ea3c0
MariaSanchezQ <81332618+MariaSanchezQ@users.noreply.github.com>
2021-08-11 23:56:18 +0000

    While e if realizados

--- py5_matplotlib/plot_list.py
+++ py5_matplotlib/plot_list.py
@@ -1,6 +1,9 @@
+print("Ejercicio 5 Python: Plot List - Matplotlib");
+print(" ");
 C=[];
 a=int(input("Ingrese un valor entero: "));
 C.append(a);
+print(" ");
 
 while (a != 1):
   if a%2 ==0:
@@ -9,5 +12,6 @@ while (a != 1):
     a = a*3 + 1;
   C.append(a);
 
-
 print(C);
+print(" ");
+print("El programa ha finalizado.");
